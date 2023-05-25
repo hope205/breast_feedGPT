cad5ccbab120914d2cb266eab0d27fad3d879626
Hope <ogidanhope1@gmail.com>
2023-05-25 12:01:10 +0100

    first

---
+++ utils.py
@@ -0,0 +1,63 @@
+import os
+import openai
+import requests
+from dotenv import load_dotenv
+import streamlit as st
+load_dotenv()
+
+import pinecone
+
+
+
+
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
+PINECONE_API_ENV = st.secrets["PINECONE_API_ENV"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+    
+    # initialize pinecone
+pinecone.init(
+api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+environment=PINECONE_API_ENV  # next to api key in console
+)
+
+# g = "nursing"
+
+index = pinecone.Index("breastfeeding")
+
+# index =pinecone.Index("nursing")
+
+# Get embeddings for a given string
+def get_embeddings_openai(text):
+    response = openai.Embedding.create(
+        input=text,
+        model="text-embedding-ada-002"
+    )
+    response = response['data']
+
+    # extract embeddings from responses0
+    return [x["embedding"] for x in response]
+
+
+# Search Pinecone for similar documents
+def semantic_search(query, **kwargs):
+    # Embed the query into a vector
+    v = get_embeddings_openai(query)
+    
+    
+    # retrieve from Pinecone
+    # xq = v['data'][0]['embedding']
+    xq = v
+    
+
+    # get relevant contexts
+    res = index.query(xq, top_k=3, include_metadata=True)
+    contexts = [
+        x['metadata']['text'] for x in res['matches']
+    ]
+    
+    return contexts
+    
+    
+
+  
